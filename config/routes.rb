dd61acc0dfed320f8f35e161a06f8064aeb846d8
izyszka <iza.wisniewska.iza@gmail.com>
2020-01-27 02:59:14 +0100

    Add homepage

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
     resources :reviews
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'movies#index'
+  root 'home#index'
 end
