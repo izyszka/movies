24af51a2f0333fcf9d245b66174a98ed4139701b
izyszka <iza.wisniewska.iza@gmail.com>
2020-01-30 02:11:09 +0100

    Add star rating with jQuery raty

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -18,3 +19,4 @@ require("channels")
 
 import './stylesheets/application'
 import './bootstrap_custom.js'
+import './jquery.raty.js'
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,25 @@
 const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
-environment.plugins.append(
+environment.plugins.prepend(
   'Provide',
   new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
     Popper: ['popper.js', 'default']
   })
 )
+
+environment.loaders.append('jquery', {
+   test: require.resolve('jquery'),
+   use: [{
+     loader: 'expose-loader',
+     options: '$',
+   }, {
+     loader: 'expose-loader',
+     options: 'jQuery',
+   }],
+ });
+ module.exports = environment;
 
 module.exports = environment
