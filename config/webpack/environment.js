98c65ecf7f9883d30376c2ae28300eaebb5b090c
izyszka <iza.wisniewska.iza@gmail.com>
2020-02-02 03:50:04 +0100

    Install vue

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,6 @@
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 
 const webpack = require('webpack')
 environment.plugins.prepend(
@@ -20,6 +22,10 @@ environment.loaders.append('jquery', {
      options: 'jQuery',
    }],
  });
- module.exports = environment;
+ environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+module.exports = environment;
 
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 module.exports = environment
